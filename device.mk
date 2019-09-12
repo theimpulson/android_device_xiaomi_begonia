06d56daa6a5f4ccc65fbcfbef951b90fb47cb29a
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-09-12 23:47:56 +0200

    begonia: Enabled updateable APEX
    Change-Id: Iae46cbf48ae5fbfa100e83e8d6ce19f225102cf3

--- device.mk
+++ device.mk
@@ -6,6 +6,9 @@
 
 DEVICE_PATH := device/redmi/begonia
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay
