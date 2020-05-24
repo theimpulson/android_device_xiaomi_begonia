85d82735e1f0ac6ebc52d5f3d5db7d0cd3770e4e
Kshitij Gupta <kshitijgm@gmail.com>
2020-05-24 17:36:44 +0530

    begonia: Create symlinks for gatekeeper
    Change-Id: I943f3d1fd00240305cad8f535f91845c239acb17
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- Android.mk
+++ Android.mk
@@ -19,4 +19,17 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),begonia)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
+include $(CLEAR_VARS)
+
+GATEKEEPER_SYMLINKS := $(TARGET_OUT_VENDOR)
+$(GATEKEEPER_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating gatekeeper symlinks: $@"
+	@mkdir -p $@/lib/hw
+	@mkdir -p $@/lib64/hw
+	@ln -sf libSoftGatekeeper.so $@/lib/hw/gatekeeper.default.so
+	@ln -sf libSoftGatekeeper.so $@/lib64/hw/gatekeeper.default.so
+
+ALL_DEFAULT_INSTALLED_MODULES += $(GATEKEEPER_SYMLINKS)
+
 endif
