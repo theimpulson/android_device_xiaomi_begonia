PixelExperience-Devices/device_xiaomi_begonia

begonia: Add DT2W service
Change-Id: Ia503b95831e138bb4f8aec5762b9f637f6d2451b

dt2w/src/co/potatoproject/dt2w/begonia/DT2WServiceBegonia.java
@@ -1,0 +1,66 @@
+package co.potatoproject.dt2w.begonia;
+
+import android.app.Service;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.provider.Settings.Secure;
+
+public class DT2WServiceBegonia extends Service {
+    private static final String TAG = "DT2WServiceBegonia";
+    private Context mContext;
+    private Handler mHandler;
+    private CustomSettingsObserver mCustomSettingsObserver;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startid) {
+        mContext = this;
+        mHandler = new Handler(Looper.getMainLooper());
+        mCustomSettingsObserver = new CustomSettingsObserver(mHandler);
+        mCustomSettingsObserver.observe();
+        mCustomSettingsObserver.update();
+        return START_STICKY;
+    }
+
+    private class CustomSettingsObserver extends ContentObserver {
+        CustomSettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Secure.getUriFor(Secure.DOUBLE_TAP_TO_WAKE),
+                    false, this, UserHandle.USER_CURRENT);
+        }
+
+        void update() {
+            int dt2wValue = Secure.getInt(mContext.getContentResolver(), Secure.DOUBLE_TAP_TO_WAKE, 0);
+            boolean dt2wEnabled = dt2wValue == 1;
+            SystemProperties.set("persist.sys.begonia.dt2w", dt2wEnabled ? "1" : "0");
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            if (uri.equals(Secure.getUriFor(Secure.DOUBLE_TAP_TO_WAKE))) {
+                update();
+            }
+        }
+    }
+}

dt2w/src/co/potatoproject/dt2w/begonia/OnBootCompleteReceiver.java
@@ -1,0 +1,14 @@
+package co.potatoproject.dt2w.begonia;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.UserHandle;
+
+public class OnBootCompleteReceiver extends BroadcastReceiver {
+    public void onReceive(Context context, Intent intent) {
+        Intent sIntent = new Intent(context, DT2WServiceBegonia.class);
+        context.startServiceAsUser(sIntent, UserHandle.CURRENT);
+    }
+}

theimpulson/android_device_xiaomi_begonia

dt2w/src/co/lineageos/dt2w/begonia/DT2WServiceBegonia.java
@@ -1,0 +1,66 @@
+package org.lineageos.dt2w.begonia;
+
+import android.app.Service;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.provider.Settings.Secure;
+
+public class DT2WServiceBegonia extends Service {
+    private static final String TAG = "DT2WServiceBegonia";
+    private Context mContext;
+    private Handler mHandler;
+    private CustomSettingsObserver mCustomSettingsObserver;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startid) {
+        mContext = this;
+        mHandler = new Handler(Looper.getMainLooper());
+        mCustomSettingsObserver = new CustomSettingsObserver(mHandler);
+        mCustomSettingsObserver.observe();
+        mCustomSettingsObserver.update();
+        return START_STICKY;
+    }
+
+    private class CustomSettingsObserver extends ContentObserver {
+        CustomSettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Secure.getUriFor(Secure.DOUBLE_TAP_TO_WAKE),
+                    false, this, UserHandle.USER_CURRENT);
+        }
+
+        void update() {
+            int dt2wValue = Secure.getInt(mContext.getContentResolver(), Secure.DOUBLE_TAP_TO_WAKE, 0);
+            boolean dt2wEnabled = dt2wValue == 1;
+            SystemProperties.set("persist.sys.begonia.dt2w", dt2wEnabled ? "1" : "0");
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            if (uri.equals(Secure.getUriFor(Secure.DOUBLE_TAP_TO_WAKE))) {
+                update();
+            }
+        }
+    }
+}

dt2w/src/co/lineageos/dt2w/begonia/OnBootCompleteReceiver.java
@@ -1,0 +1,14 @@
+package org.lineageos.dt2w.begonia;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.UserHandle;
+
+public class OnBootCompleteReceiver extends BroadcastReceiver {
+    public void onReceive(Context context, Intent intent) {
+        Intent sIntent = new Intent(context, DT2WServiceBegonia.class);
+        context.startServiceAsUser(sIntent, UserHandle.CURRENT);
+    }
+}